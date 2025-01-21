115133f623ea49ae2653bfeeb92c049c701e4bac
KSalmaze <pedrohsalmaze@gmail.com>
2025-01-21 00:51:36 -0300

    Browser manager class

--- Autoclicker.py
+++ Autoclicker.py
@@ -6,46 +6,51 @@ from selenium.webdriver.chrome.options import Options
 # TODO
 # - Paralelizar a abertura de janelas
 
-def open_sites():
+class BrowserManager:
 
-    # links
-    tiktok_url = "https://www.tiktok.com"
-    kwai_url = "https://www.kwai.com"
-    youtube_shorts_url = "https://www.youtube.com/shorts"
+    def __init__(self):
 
-    # Resolução da tela
-    root = tkinter.Tk()
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-    root.destroy()
+    def open_sites(self):
 
-    # Dimensões das janelas
-    window_width = screen_width // 3
-    window_height = screen_height
+        # links
+        tiktok_url = "https://www.tiktok.com"
+        kwai_url = "https://www.kwai.com"
+        youtube_shorts_url = "https://www.youtube.com/shorts"
 
-    thread1 = threading.Thread(target=setup_driver, args=(tiktok_url, 0, window_width, window_height))
-    thread2 = threading.Thread(target=setup_driver, args=(kwai_url, window_width, window_width, window_height))
-    thread3 = threading.Thread(target=setup_driver,
-                               args=(youtube_shorts_url, window_width * 2, window_width, window_height))
+        # Resolução da tela
+        root = tkinter.Tk()
+        screen_width = root.winfo_screenwidth()
+        screen_height = root.winfo_screenheight()
+        root.destroy()
 
-    # Inicia as threads
-    thread1.start()
-    thread2.start()
-    thread3.start()
+        # Dimensões das janelas
+        window_width = screen_width // 3
+        window_height = screen_height
 
-    # Aguarda todas as threads terminarem
-    thread1.join()
-    thread2.join()
-    thread3.join()
+        thread1 = threading.Thread(target=self.setup_driver, args=(tiktok_url, 0, window_width, window_height))
+        thread2 = threading.Thread(target=self.setup_driver, args=(kwai_url, window_width, window_width, window_height))
+        thread3 = threading.Thread(target=self.setup_driver, args=(youtube_shorts_url, window_width * 2, window_width, window_height))
+
+        # Inicia as threads
+        thread1.start()
+        thread2.start()
+        thread3.start()
+
+        # Aguarda todas as threads terminarem
+        thread1.join()
+        thread2.join()
+        thread3.join()
 
 
-def setup_driver(url, position_x, window_width, window_height):
-    chrome_options = Options()
-    chrome_options.add_experimental_option("detach", True)
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.get(url)
-    driver.set_window_size(window_width, window_height)
-    driver.set_window_position(position_x, 0)
+    def setup_driver(self,url, position_x, window_width, window_height):
+        chrome_options = Options()
+        chrome_options.add_experimental_option("detach", True)
+        driver = webdriver.Chrome(options=chrome_options)
+        driver.get(url)
+        driver.set_window_size(window_width, window_height)
+        driver.set_window_position(position_x, 0)
+
+browser_manager = BrowserManager()
 
 # Criar janela principal
 window = tkinter.Tk()
@@ -54,7 +59,7 @@ window.geometry("500x500")
 window.configure(bg='#bdb9b9')
 
 # Criar os botões
-button = tkinter.Button(window, text="ABRIR SITES", command=open_sites)
+button = tkinter.Button(window, text="ABRIR SITES", command=browser_manager.open_sites)
 button.pack(side = "top", pady = 25)
 
 # Loop principal
